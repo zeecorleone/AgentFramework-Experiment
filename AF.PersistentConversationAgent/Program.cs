8d29d9488701188f2d5cd65a79071a4dd59213cc
Zeeshan Mustafa <zeeshan.mustafa91@gmail.com>
2026-01-13 09:56:20 +0500

    Agnet Threads - Chat Persistence

---
+++ AF.PersistentConversationAgent/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using AF.Shared.Extensions;
+using AFExperiments;
+using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI.Chat;
+
+AzureOpenAIClient client = new AzureOpenAIClient(
+    new Uri(Constants.Endpoint_AzureOpenAI),
+    new System.ClientModel.ApiKeyCredential(Constants.ApiKey));
+
+var chatClient = client.GetChatClient(Constants.Model);
+AIAgent agent = chatClient.CreateAIAgent("You are a friendly AI Bot, answering my questions.");
+
+AgentThread thread = agent.GetNewThread();
+
+var resumeOldChat = true;
+
+if(resumeOldChat)
+{
+    thread = await AgentThreadPersistenceFileBased.ResumeChatIfRequestedAsync(agent);
+}
+
+while(true)
+{
+    Console.Write("User>>: ");
+    string userInput = Console.ReadLine();
+
+    if(!string.IsNullOrEmpty(userInput))
+    {
+        Microsoft.Extensions.AI.ChatMessage message = new Microsoft.Extensions.AI.ChatMessage(ChatRole.User, userInput);
+        await foreach(AgentRunResponseUpdate update in agent.RunStreamingAsync(message, thread))
+        {
+            Console.Write(update);
+        }
+        
+        Console.WriteLine("\n" + string.Empty.PadLeft(50, '*') + "\n");
+        await AgentThreadPersistenceFileBased.StoreThreadAsync(thread);
+
+    }
+    
+}
---
+++ AF.Shared/Extensions/AgentThreadPersistenceFileBased.cs
@@ -0,0 +1,61 @@
+﻿
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using System.Text.Json;
+
+namespace AF.Shared.Extensions;
+
+public static class AgentThreadPersistenceFileBased
+{
+    private static string ConversationPath => Path.Combine(@"D:\garbage\agent-framework\agent-persistence", "conversation.json");
+
+    public static async Task<AgentThread> ResumeChatIfRequestedAsync(AIAgent agent)
+    {
+        if (File.Exists(ConversationPath))
+        {
+            Console.Write("Restore previous conversation? (Y/N): ");
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.Clear();
+            if (key.Key == ConsoleKey.Y)
+            {
+                var fileContent = await File.ReadAllTextAsync(ConversationPath);
+                JsonElement jsonElement = JsonSerializer.Deserialize<JsonElement>(fileContent);
+                AgentThread resumedThread = agent.DeserializeThread(jsonElement);
+
+                await RestoreConsole(resumedThread);
+                return resumedThread;
+            }
+        }
+
+        return agent.GetNewThread();
+    }
+
+    private static async Task RestoreConsole(AgentThread resumedThread)
+    {
+        ChatClientAgentThread chatClientAgentThread = (ChatClientAgentThread)resumedThread;
+        if (chatClientAgentThread.MessageStore != null)
+        {
+            IList<ChatMessage>? messages = resumedThread.GetService<IList<ChatMessage>>();
+            foreach (ChatMessage message in messages!)
+            {
+                if (message.Role == ChatRole.User)
+                {
+                    Console.WriteLine($"User>>: {message.Text}");
+                }
+                else if (message.Role == ChatRole.Assistant)
+                {
+                    Console.WriteLine($"{message.Text}");
+                    Console.WriteLine();
+                    Console.WriteLine(string.Empty.PadLeft(50, '*'));
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+    public static async Task StoreThreadAsync(AgentThread thread)
+    {
+        JsonElement serializedThread = thread.Serialize();
+        await File.WriteAllTextAsync(ConversationPath, JsonSerializer.Serialize(serializedThread));
+    }
+}
