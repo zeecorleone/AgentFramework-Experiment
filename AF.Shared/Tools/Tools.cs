4c4e5d3747ccfd26696c70dec5394b6c4dc8e7df
Zeeshan Mustafa <zeeshan.mustafa91@gmail.com>
2026-01-13 11:40:22 +0500

    Tools Calling in agent (Basics)

---
+++ AF.Shared/Tools/Tools.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.ComponentModel;
+
+namespace AF.Shared.Tools;
+
+public static class Tools
+{
+    [Description("Gets the current date and time.")] //--> side note: everything cost tokens, so if your tool name is descriptive enough, you might not need this attribute.
+    public static DateTime CurrentDateAndTime(TimeType type)
+    {
+        return type switch
+        {
+            TimeType.Local => DateTime.Now,
+            TimeType.Utc => DateTime.UtcNow,
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+    
+    [Description("Gets the current time zone.")]
+    public static string CurrentTimeZone()
+    {
+        return TimeZoneInfo.Local.StandardName;
+    }
+}
+
+public enum TimeType
+{
+    Local,
+    Utc
+}
---
+++ AF.ToolCalling.Basics/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AF.Shared.Tools;
+using AFExperiments;
+using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI.Chat;
+
+Console.WriteLine("Hello, World!");
+
+
+AzureOpenAIClient client = new AzureOpenAIClient(
+    new Uri(Constants.Endpoint_AzureOpenAI),
+    new System.ClientModel.ApiKeyCredential(Constants.ApiKey));
+
+ChatClient chatClient = client.GetChatClient(Constants.Model);
+
+//AIAgent agent = chatClient.CreateAIAgent("You are a Time expert agent, answering questions");
+
+AIAgent agent = chatClient.CreateAIAgent(
+    instructions: "You are a Time expert agent.",
+    tools:
+        [
+            AIFunctionFactory.Create(Tools.CurrentDateAndTime, "current_date_time"),
+            AIFunctionFactory.Create(Tools.CurrentTimeZone, "current_time_zone")
+        ]
+    );
+
+AgentThread thread = agent.GetNewThread();
+
+
+while(true)
+{
+    Console.Write("User>>: ");
+    string userInput = Console.ReadLine();
+    if (!string.IsNullOrEmpty(userInput))
+    {
+        Microsoft.Extensions.AI.ChatMessage message = new Microsoft.Extensions.AI.ChatMessage(ChatRole.User, userInput);
+        await foreach (AgentRunResponseUpdate update in agent.RunStreamingAsync(message, thread))
+        {
+            Console.Write(update);
+        }
+        Console.WriteLine("\n" + string.Empty.PadLeft(50, '*') + "\n");
+    }
+
+}
